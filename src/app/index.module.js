9a7779555284a50fd3a9ba8b94bf66c6aea15276
Nebrass Lamouchi <lnibrass@gmail.com>
2016-01-23 19:28:27 +0100

    Initial commit

---
+++ src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('yeomanGeneratedProject', ['ngCookies', 'restangular', 'ui.router', 'ui.bootstrap', 'toastr']);
+
+})();
---
+++ src/app/index.run.js
@@ -0,0 +1,14 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('yeomanGeneratedProject')
+    .run(runBlock);
+
+  /** @ngInject */
+  function runBlock($log) {
+
+    $log.debug('runBlock end');
+  }
+
+})();
